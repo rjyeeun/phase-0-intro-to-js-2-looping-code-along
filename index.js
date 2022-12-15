258baed60016a40ebf361aaadc1c6cf130c6d9c2
Yeeun Jung <rjyeeun@gmail.com>
2022-12-15 13:51:37 -0500

    commit tree 1786ae5298acb4d1aa1cbad65144e48f2e335f61

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+const  gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+    for (let i = 0; i <gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+}
+return gifts;
+}
+
+wrapGifts(gifts) 
+
+// create a new empty array to hold the messages
+
+
+
+
+//iterate through the input array and, inside the loop, build out the 'thank you' 
+//message for each name using string interpolation, then add that message to the 
+//new array you created;
+function writeCards(names, events) {
+    const array = [];
+    for (let i = 0 ; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return array;
+}
+
+function countDown() {
+    let countDown = 0; while (countDown < 11) {
+        console.log(countDown++)
+    }
+}
